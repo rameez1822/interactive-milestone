b445c120aad0b2cc0a65646f32d50ecfd836bf2b
rameez1822 <53996659+rameez1822@users.noreply.github.com>
2020-04-08 01:36:05 +0200

    map and autocomplete

--- interactive-project/js/map.js
+++ interactive-project/js/map.js
@@ -1,4 +1,3 @@
-function activatePlacesSearch(){
-    var input = document.getElementById("auto-complete")
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
+<script>
+</script>
+ 
