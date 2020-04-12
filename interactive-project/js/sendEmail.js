51a567f9afd717118637b2bcf2eea0ef05b30acf
rameez1822 <53996659+rameez1822@users.noreply.github.com>
2020-04-12 22:00:56 +0200

    cards and carousel fixed

--- interactive-project/js/sendEmail.js
+++ interactive-project/js/sendEmail.js
@@ -1,1 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "yummy_bites", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "Contact_Us": contactForm.contactUs.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
 console.log("hello")
